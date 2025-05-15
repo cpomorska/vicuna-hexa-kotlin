c393f9574bc0d5e00c7dac3b2cb0eb4482b21d1a
cpomorska <claudius.pomorska@googlemail.com>
2025-05-15 21:30:43 +0200

    Add H2 database support for UserHandlingEventFactoryTest

--- src/test/kotlin/com/scprojekt/domain/model/user/UserHandlingEventFactoryTest.kt
+++ src/test/kotlin/com/scprojekt/domain/model/user/UserHandlingEventFactoryTest.kt
@@ -4,6 +4,8 @@ import com.scprojekt.domain.model.user.event.UserEventFactory
 import com.scprojekt.domain.model.user.event.UserEventType
 import com.scprojekt.domain.model.user.event.UserHandlingEvent
 import com.scprojekt.util.TestUtil.Companion.createTestUser
+import io.quarkus.test.common.WithTestResource
+import io.quarkus.test.h2.H2DatabaseTestResource
 import io.quarkus.test.junit.QuarkusTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Disabled
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test
 
 
 @QuarkusTest
+@WithTestResource(H2DatabaseTestResource::class)
 class UserHandlingEventFactoryTest {
 
     @Test
